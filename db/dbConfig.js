e9b7e84b3c99e21e60622ab02a572de407756182
getahunabr <147873183+getahunabr@users.noreply.github.com>
2024-03-11 12:06:51 -0700

    backend

---
+++ db/dbConfig.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2/promise');
+
+const dbConnection = mysql.createPool({
+    user:process.env.USER,
+    database:process.env.DATABASE,
+    host: "localhost",
+    password: process.env.PASSWORD,
+    connectionLimit:10
+})
+
+
+
+
+module.exports = dbConnection;
+
+// dbConnection.execute("select 'test'", (err,result)=>{
+//     if(err){
+//         console.log(err.message)
+//     }else{
+//         console.log(result)
+//     }
+// })
